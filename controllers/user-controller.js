dbffee5d535762f7ac104f90251aba1d26642601
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-12 16:31:57 -0700

    added getAllUser() to user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,20 @@ const { User } = require('../models');
 
 const userController = {
   // get all users
+  getAllUser(req, res) {
+    User.find({})
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 })
+      .then(dbUserData => res.json(dbUserData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
   // get user by id (populate thought and friend data)
   // POST a new user
   createUser({ body }, res) {
