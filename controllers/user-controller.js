8671adae8ffa66287d26baaa421f1f2d07e0e50d
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-12 16:59:29 -0700

    added updateUser() and deleteUser() to user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,4 @@
-const { User } = require('../models');
+const { User, Thought } = require('../models');
 
 const userController = {
   // get all users
@@ -45,10 +45,36 @@ const userController = {
     User.create(body)
       .then(dbUserData => res.json(dbUserData))
       .catch(err => res.status(400).json(err));
-  }
+  },
   // PUT update user by id
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => res.status(400).json(err));
+  },
   // DELETE user by id
-  // BONUS: remove a user's associated thoughts when deleted
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        // BONUS: remove a user's associated thoughts when deleted
+        Thought.deleteMany({  username: dbUserData.username })
+          .then((dbUserData) => {
+            res.json({ message: `Successfully deleted ${dbUserData.username}!` });
+          })
+          .catch(err => res.status(400).json(err));
+      })
+      .catch(err => res.status(400).json(err));
+  }
 };
 
 module.exports = userController;
