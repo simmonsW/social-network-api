d72ce644be9f4339e5af58f80a33533d51bdba24
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-12 16:36:04 -0700

    added getUserById() to user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -17,6 +17,29 @@ const userController = {
       });
   },
   // get user by id (populate thought and friend data)
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'thoughts',
+        select: '-__v'
+      })
+      .populate({
+        path: 'friends',
+        select: '-__v'
+      })
+      .select('-__v')
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+  },
   // POST a new user
   createUser({ body }, res) {
     User.create(body)
