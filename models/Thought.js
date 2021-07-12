1d4d4138e4e27dcccb7aaca9cd4b17443f5e5bb8
Wyatt Simmons <simmonsw182@gmail.com>
2021-07-12 16:09:23 -0700

    set up ReactionSchema, and set up index.js

--- models/Thought.js
+++ models/Thought.js
@@ -2,7 +2,32 @@ const { Schema, model, Types } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
 const ReactionSchema = new Schema(
-
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => newTypes.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
 );
 
 const ThoughtSchema = new Schema(
